7b03a2bf8c3f894e6dd9f3f9281fc8d58a1b3354
Saleh Masoudi <m.smasoudi72@gmail.com>
2026-08-11 21:44:00 +0330

    feat: added interrupt for control blinking led while pressing a button;

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,12 +7,19 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use core::cell::RefCell;
 use core::fmt::Write;
 use core::panic::PanicInfo;
+use core::sync::atomic::{AtomicBool, AtomicI32, AtomicU32, Ordering};
+use critical_section::Mutex;
 use esp_hal::clock::CpuClock;
-use esp_hal::gpio::{Level, Output, OutputConfig};
-use esp_hal::main;
+use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
+use esp_hal::interrupt::software::SoftwareInterruptControl;
+use esp_hal::time::Duration;
+use esp_hal::timer::timg::TimerGroup;
+use esp_hal::timer::PeriodicTimer;
 use esp_hal::uart::{Config, Uart};
+use esp_hal::{handler, main, ram};
 use esp_start::delay;
 
 #[panic_handler]
@@ -23,13 +30,39 @@ fn panic(_: &PanicInfo) -> ! {
 // Don't Remove This Code! Code for bootloader
 esp_bootloader_esp_idf::esp_app_desc!();
 
+static TIMER_COUNTER: AtomicU32 = AtomicU32::new(0);
+static TIMER_DELAY: AtomicU32 = AtomicU32::new(500);
+static TIMER_FIRED: AtomicBool = AtomicBool::new(false);
+static TIMER: Mutex<RefCell<Option<PeriodicTimer<'static, esp_hal::Blocking>>>> =
+    Mutex::new(RefCell::new(None));
+
+#[handler]
+fn timer_handler() {
+    critical_section::with(|cs| {
+        let mut timer = TIMER.borrow_ref_mut(cs);
+        if let Some(timer) = timer.as_mut() {
+            timer.clear_interrupt();
+        }
+    });
+
+    TIMER_FIRED.store(true, Ordering::Relaxed);
+    TIMER_COUNTER.fetch_add(1, Ordering::Relaxed);
+}
+
+// ######################
+
 #[allow(
     clippy::large_stack_frames,
     reason = "it's not unusual to allocate larger buffers etc. in main"
 )]
 #[main]
 fn main() -> ! {
+    // esp_alloc::heap_allocator!(#[ram(reclaimed)] size: 64 * 1024);
+    // esp_alloc::heap_allocator!(size: 36 * 1024);
+
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let pullup_btn_cfg = InputConfig::default().with_pull(Pull::Up);
+
     let _peripherals = esp_hal::init(config);
 
     let mut uart = Uart::new(_peripherals.UART0, Config::default())
@@ -38,24 +71,69 @@ fn main() -> ! {
         .with_rx(_peripherals.GPIO3);
 
     let mut led = Output::new(_peripherals.GPIO23, Level::Low, OutputConfig::default());
+    let mut btn_led = Output::new(_peripherals.GPIO21, Level::Low, OutputConfig::default());
+    let test_btn = Input::new(_peripherals.GPIO22, pullup_btn_cfg);
 
+    uart.write_str("Setup timer ...\r\n").unwrap();
+    // let sw_interrupt = SoftwareInterruptControl::new(_peripherals.SW_INTERRUPT);
 
-    uart.write_str("==== SALEH ESP32 ====\r\n\tHello from Rust on ESP32!\r\n")
-        .unwrap();
+    let timg0 = TimerGroup::new(_peripherals.TIMG0);
+    let mut timer = PeriodicTimer::new(timg0.timer0);
+    timer.set_interrupt_handler(timer_handler);
 
-    let mut mulp = 1;
+    critical_section::with(|cs| {
+        TIMER.borrow_ref_mut(cs).replace(timer);
+    });
+    critical_section::with(|cs| {
+        let mut timer = TIMER.borrow_ref_mut(cs);
+        let timer = timer.as_mut().unwrap();
 
+        timer.start(Duration::from_millis(TIMER_DELAY.load(Ordering::Relaxed) as u64)).unwrap();
+        timer.listen();
+    });
+
+    // esp_rtos::start(timg0.timer1, sw_interrupt.software_interrupt0);
+
+    // uart.write_str("Prepare wifi ...").unwrap();
+    // let mut wifi = match WifiController::new(_peripherals.WIFI, Default::default()) {
+    //     Ok(wifi) => {
+    //         uart.write_str("WiFi controller initialized!\r\n").unwrap();
+    //         wifi
+    //     }
+    //     Err(_) => {
+    //         uart.write_str("WiFi init FAILED!\r\n").unwrap();
+    //
+    //         // ERR LED Blinking
+    //         TIMER_DELAY.store(150, Ordering::Relaxed);
+    //         loop {
+    //             if TIMER_FIRED.swap(false, Ordering::Relaxed) {
+    //                 uart.write_str("toggle led!\r\n").unwrap();
+    //                 led.toggle();
+    //             }
+    //         }
+    //     }
+    // };
+
+    let mut last_event_call_count = 0;
     loop {
+        let counter = TIMER_COUNTER.load(Ordering::Relaxed);
+        let mut i = 0;
+        while i < counter.wrapping_sub(last_event_call_count) {
+            write!(uart, "L{}: toggle led!\r\n", last_event_call_count + i + 1).unwrap();
+            led.set_high();
+            delay(50);
+            led.set_low();
+            delay(50);
+            i += 1
+        }
+        last_event_call_count = counter;
 
-        led.set_high();
-        uart.write_str("LED -> ON\r\n").unwrap();
-        delay(10 * mulp);
-
-        led.set_low();
-        uart.write_str("LED -> OFF\r\n").unwrap();
-        delay(10 * mulp);
-
-        mulp += 1;
-        write!(uart, "mulp = {}\r\n", mulp).unwrap();
+        if test_btn.is_low() {
+            uart.write_str("set btn is down\r\n").unwrap();
+            btn_led.set_high();
+        } else {
+            btn_led.set_low();
+        }
+        delay(10);
     }
 }
